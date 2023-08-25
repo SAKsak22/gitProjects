53a36bf61904f64a5eab7180d709dea43300ceb7
is <dltkr9841@naver.com>
2023-08-25 17:30:32 +0900

    ??

--- testProject/src/com/kg/extendsjava/DAY0826.java
+++ testProject/src/com/kg/extendsjava/DAY0826.java
@@ -1,6 +1,27 @@
 package com.kg.extendsjava;
-import java.util
-public class DAY0826 {
+import java.util.Scanner;
 
+public class DAY0826 {
+	 public static void main(String[] args) {
+
+	Scanner sc = new Scanner(System.in);
 	
+	int num;
+	
+	
+	
+	System.out.print("정수를 입력하세요 : ");
+		num = sc.nextInt();
+		if(num>= 1 && num <=10) {
+			for(int i = 1; i<num; i ++) {
+				if(i%2==1) {
+					System.out.print(i +" ");
+				}
+			}
+		}
+		
+		
+	
+	
+	 }
 }
